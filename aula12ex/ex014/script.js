a8a09122656a200fa39b3a99b5c825a0442c6c5f
jefferson092803 <128716417+jefferson092803@users.noreply.github.com>
2023-11-21 17:16:03 -0300

    exercicio 14 feito

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -1,1 +1,22 @@
-alert("Ola");
+function carregar() {
+    var msg= window.document.getElementById('msg')
+    var img= window.document.getElementById('imagem')
+    var data= new Date()
+    var hora= data.getHours()
+    var min=data.getMinutes()
+    msg.innerHTML = `Agora sao ${hora} horas e ${min} minutos.`
+    if (hora>=0 && hora < 12 ) {
+        //Bom Dia
+        img.src = 'imagens/manhã.jpg'
+        document.body.style.background='#BFC6D0'
+    }else if (hora >= 12 && hora < 18) {
+        //Boa Tarde
+        img.src= 'imagens/tarde.jpg'
+        document.body.style.background='#CF5500'
+    }else{
+        //Boa noite
+        img.src= 'imagens/noite.jpg'
+        document.body.style.background='#242B3D'
+    }
+}
+  
