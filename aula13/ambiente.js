85221e6695b125eeada5e90fcb970eb68a48b6b6
jeffersonLucass <jeffersonlucas810@gmail.com>
2024-01-24 09:17:11 -0300

    aprendi while e do while em javascript

---
+++ aula13/ambiente.js
@@ -0,0 +1,31 @@
+var c = 1
+do{
+    console.log('Tudo bem?')
+    c++
+}while (c <= 6)
+   
+
+
+
+
+
+
+/* ENQUANTO
+while (c <= 6) {
+    console.log('Tudo bem?')
+    c++
+}
+*/
+
+
+
+
+
+/*
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+*/
