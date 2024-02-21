cbef1b1b05b02348d6a526b6376af4b110872f65
jeffersonLucass <jeffersonlucas810@gmail.com>
2024-02-21 19:35:16 -0300

    .

--- aula14ex/ex17/modelo/script.js
+++ aula14ex/ex17/modelo/script.js
@@ -1,1 +1,20 @@
-alert("Ola");
+function tabuada() {
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+    if (num.value.length == 0) {
+       alert('Por favor,digite um número!')
+    }else{
+        let n = Number(num.value)
+        let c=1;
+        //LIMPAR A TABUADA ANTES DE SER MOSTRADA
+        tab.innerHTML=''
+        while (c<=10) {
+           let item = document.createElement('option') 
+           item.text = `${n} x ${c} = ${n*c}`
+           //ADICIONAR ELEMENTO FILHO A TABUADA
+           tab.appendChild(item)
+           c++
+        }
+    }
+    
+}
