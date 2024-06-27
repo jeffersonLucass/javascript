3635c9bd2ad465fe802c6ccb16bd6021c1c74b8e
jeffersonLucass <jeffersonlucas810@gmail.com>
2024-06-27 09:06:43 -0300

    ..

---
+++ PROJETOS/8_boolean_op_logico/script.js
@@ -0,0 +1,19 @@
+console.log(5 > 3 && 3 == 2)
+console.log(5 > 3 || 3 == 1)
+
+
+console.log(3==3 && 'matheus'=='matheus')
+console.log('felipe'=='joao' || false)
+
+
+console.log(!(!(true && true)))
+//no caso a utilização do and só irá retornar verdadeiro caso os dois lados forem verdadeiros
+console.log(true && true)
+console.log(false ||false)
+//já o OU irá retornar verdadeiro a partir de um lado verdadeiro nao tendo a necessidade de todos os lados serem verdadeiros..
+
+
+
+
+
+
---
+++ PROJETOS/9_boolean_op_ternario/script.js
@@ -0,0 +1,10 @@
+console.log(5 > 2 ? 'é sim' : 'é não')
+console.log(false ? 5 : 4)
+console.log("Matheus" =="Jõao" ? "Olá,Matheus!": "Não é o Matheus!")
+
+
+
+
+
+
+
