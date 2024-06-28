98696f0ae230ee382baf78e319e6c318a8375343
jeffersonLucass <jeffersonlucas810@gmail.com>
2024-06-28 10:01:24 -0300

    ..

---
+++ PROJETOS/2_topico/1_variaveis/script.js
@@ -0,0 +1,19 @@
+let nome="jefferson"
+
+console.log(nome)
+console.log(`Meu nome é ${nome}.`)
+
+
+let laranjas = 5
+console.log(laranjas * laranjas)
+console.log(`Eu tenho ${laranjas} laranjas.`)
+nome = "joao"
+console.log(nome)
+console.log(`Meu nome é ${nome}.`)
+laranjas++
+console.log(`Eu tenho ${laranjas} laranjas.`)
+console.log(laranjas)
+
+let um=1,dois=2,tres=3
+
+console.log(um + dois + tres)
