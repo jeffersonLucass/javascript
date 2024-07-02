177818f9b64068350b0ce24698eb0c0f2981c027
jeffersonLucass <jeffersonlucas810@gmail.com>
2024-07-02 09:53:48 -0300

    ..

---
+++ PROJETOS/2_topico/6_math/script.js
@@ -0,0 +1,15 @@
+let MaiorNum= Math.max(1,2,345,5,5)
+console.log(MaiorNum)
+
+let MenorNum = Math.min(1,2,345,5,5)
+console.log(MenorNum)
+
+let arredondar = Math.round(5.2381379163)
+console.log(arredondar)
+
+let arredondarBaixo = Math.floor(5.75)
+
+console.log(arredondarBaixo)
+
+let arredondarParaCima=Math.ceil(5.2381379163)
+console.log(arredondarParaCima)
---
+++ PROJETOS/2_topico/7_console_log/script.js
@@ -0,0 +1,6 @@
+let idade=28
+let nome="matheus"
+
+ console.log("Nome: " + nome)
+ console.log("Idade: " + idade)
+ console.log(`Nome: ${nome}, Idade: ${idade}`)
