c4a9553cf65210c4572ed6b1fbb4603afa271b94
jeffersonLucass <jeffersonlucas810@gmail.com>
2024-07-03 08:16:28 -0300

    ..

---
+++ PROJETOS/2_topico/10_else/scripts.js
@@ -0,0 +1,8 @@
+let velocidade =100
+
+if (velocidade <80) {
+    console.log("não foi multado!!")
+}
+else{
+    console.log("foi multado!!")
+}
---
+++ PROJETOS/2_topico/11_else_if/scripts.js
@@ -0,0 +1,20 @@
+let nome = "Matheus"
+let idade = 28
+
+if (nome != undefined && nome == "Joaquim"){
+    console.log("Nome está definido")
+}
+else if (nome == "Matheus" && nome.length > 5 && idade == 50){
+    console.log("O nome é Matheus")
+}
+ else{
+    console.log("Nome não está definido ou não é Matheus")
+}
+
+
+
+if (1 > 2) {
+    console.log("teste")
+} else if(1 == 1){
+    console.log("testando")
+}
