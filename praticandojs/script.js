d4a60df5c9ae3b1b8f97e7dc1a2702a515506f24
jeffersonLucass <jeffersonlucas810@gmail.com>
2024-07-03 08:54:55 -0300

    ..

--- praticandojs/script.js
+++
@@ -1,6 +0,0 @@
-let nome1=window.prompt("Qual seu nome?");
-         if (nome1!=null && nome1!='') {
-             document.write(`Ola,${nome1}!!!`)
-         }else{
-            document.write('Ola,visitante')
-         }
