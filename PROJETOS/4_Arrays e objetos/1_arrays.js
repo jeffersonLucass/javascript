e8ed72b40560d21cbc6eaebeb0c5e50a56cc57c8
jeffersonLucass <jeffersonlucas810@gmail.com>
2024-07-18 08:45:02 -0300

    ....

---
+++ PROJETOS/4_Arrays e objetos/1_arrays.js
@@ -0,0 +1,10 @@
+let arr = [1 , 4, 5, 6, 7]
+let nome=["Jefferson", "Joaquim", "Roberta"]
+let bool = [true, false, true]
+
+let misturado = [1, "Jefferson",true]
+
+console.log(arr)
+console.log(nome)
+console.log(bool)
+console.log(misturado)
---
+++ PROJETOS/4_Arrays e objetos/2_mais_sobre_arrays.js
@@ -0,0 +1,11 @@
+let arr = [1 , 4, 5, 6, 7]
+let nome=["Jefferson", "Joaquim", "Roberta"]
+let bool = [true, false, true]
+
+let misturado = [1, "Jefferson",true]
+
+
+console.log(arr[1])
+console.log(nome[0])
+console.log(bool[2])
+console.log(arr[arr.length - 1])  //acessa o último elemento do array
