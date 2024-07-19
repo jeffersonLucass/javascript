c8809ede3421d7bb21000e2f7e441c5d617b13aa
jeffersonLucass <jeffersonlucas810@gmail.com>
2024-07-19 09:21:52 -0300

    ..

---
+++ PROJETOS/4_Arrays e objetos/4_metodos.js
@@ -0,0 +1,13 @@
+let marca = 'nike'
+
+console.log(marca.toUpperCase())
+
+let marca2 = marca.toUpperCase()
+
+console.log(marca2.toLowerCase())
+
+
+let number = 10101010
+console.log(typeof (marca2.toLowerCase))
+
+console.log(number.toString())
