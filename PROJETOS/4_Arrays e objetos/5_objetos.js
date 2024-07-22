84894582c4cc0f902f3e5b9cd884fd41eb881b98
jeffersonLucass <jeffersonlucas810@gmail.com>
2024-07-22 08:28:22 -0300

    ....

---
+++ PROJETOS/4_Arrays e objetos/5_objetos.js
@@ -0,0 +1,17 @@
+let cachorro = {
+    patas:4,
+    nome:'shark',
+    latir:() => {
+        console.log("Au Au Au")
+    }
+    
+}
+
+
+console.log(cachorro.nome)
+console.log(cachorro.patas)
+console.log(cachorro.latir())
+
+
+//[] = array
+//{} = objeto
---
+++ PROJETOS/4_Arrays e objetos/6_obj_criar_prop.js
@@ -0,0 +1,18 @@
+let pessoa = {
+    nome:"Matheus",
+    idade:29,
+    profissao:"programador"
+}
+
+
+console.log(pessoa.nome)
+delete pessoa.nome
+
+console.log(pessoa.nome)
+
+
+pessoa.casado = false
+
+console.log(pessoa.casado)
+
+console.log(pessoa)
