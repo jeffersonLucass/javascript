4cb2ca9b176c145bd372c6c5ed11df86b768bb57
jeffersonLucass <jeffersonlucas810@gmail.com>
2024-09-18 09:42:26 -0300

    ....

---
+++ PROJETOS2/11_EventosJs/06_eventosDeTecla/scripts.js
@@ -0,0 +1,11 @@
+window.addEventListener("keydown", function(e){
+    if(e.key == "q"){
+        console.log("Apertou a tecla Q");
+    }
+})
+
+window.addEventListener("keyup", function(e){
+    if(e.key == "Enter"){
+        console.log("Soltou o Enter");
+    }
+})
