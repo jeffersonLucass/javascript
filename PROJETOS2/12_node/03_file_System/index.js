d41e51891f20715753b518c2ca60a00d58000d07
jeffersonLucass <jeffersonlucas810@gmail.com>
2024-10-04 14:50:25 -0300

    ...

---
+++ PROJETOS2/12_node/03_file_System/index.js
@@ -0,0 +1,19 @@
+const { error } = require('console');
+let {readFile,writeFile} = require('fs');
+
+readFile('arquivo.txt', 'utf8', (error, texto) => {
+   if (error) {
+        throw error;
+   } else{
+        console.log(texto);
+   }
+});
+
+
+writeFile('arquivo.txt','texto por WriteFile', (error) =>{
+    if(error){
+        throw error;
+    }else{
+        console.log('Escreveu com sucesso');
+    }
+})
