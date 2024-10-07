73f14260577fa11ed565de0d676406b1aad5aafb
jeffersonLucass <jeffersonlucas810@gmail.com>
2024-10-07 14:58:03 -0300

    Aprendi a criar uma api utilizando express e verificar ela no postman.

---
+++ PROJETOS2/12_node/05_express/index.js
@@ -0,0 +1,12 @@
+let express = require('express');
+let app = express();
+
+app.get('/', function(request, response){
+    response.send('Primeira rota com express');
+
+});
+
+
+app.listen(3000,function(){
+    console.log("A aplicação esta funcionando na porta 3000");
+});
