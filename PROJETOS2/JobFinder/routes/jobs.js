24af28e31382ceef3413fcfed69de2da90dffa1c
jeffersonLucass <jeffersonlucas810@gmail.com>
2024-10-16 09:01:12 -0300

    deu tudo errado

--- PROJETOS2/JobFinder/routes/jobs.js
+++ PROJETOS2/JobFinder/routes/jobs.js
@@ -1,23 +1,46 @@
-const express     = require('express');
-const router      = express.Router();
-const Job         = require('../models/Job');
+const express = require('express');
+const router  = express.Router();
+const Job     = require('../models/Job');
 
-//rota de teste
+// rota de teste
+router.get('/test', (req, res) => {
+  res.send('deu certo');
+});
 
-router.get('/test',(req,res) => {
-    res.send('deu certo');
-}); 
+// detalhe da vaga -> view/1, view/2
+router.get('/view/:id', (req, res) => Job.findOne({
+  where: {id: req.params.id}
+}).then(job => {
 
-//add job via post
+  res.render('view', {
+    job
+  });
 
-router.post('/add',(req,res) => {
-    
-    let {title, description, salary, company, email,new_job} = req.body; 
+}).catch(err => console.log(err)));
 
-    //insert
-    Job.create({title, description, salary, company, email,new_job}).then(() =>res.redirect('/'))
-    .catch(err => console.log(err));
 
+// form da rota de envio
+router.get('/add', (req, res) => {
+  res.render('add');
+})
+
+// add job via post
+router.post('/add', (req, res) => {
+
+  let {title, salary, company, description, email, new_job} = req.body;
+
+  // insert
+  Job.create({
+    title,
+    description,
+    salary,
+    company,
+    email,
+    new_job
+  })
+  .then(() => res.redirect('/'))
+  .catch(err => console.log(err));
 
 });
-module.exports = router;
+
+module.exports = router
